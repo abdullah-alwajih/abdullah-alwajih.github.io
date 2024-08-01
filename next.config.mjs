8da6fa177eddbead8b83f47878f8101d1b0705cd
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2024-08-01 17:07:33 +0300

    change NextJS config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  assetPrefix: '/',
+
 };
 
 export default nextConfig;
